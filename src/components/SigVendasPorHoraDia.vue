603764f2220e1dfa9eddcf9591e7faa7732dade5
fael_job <rafael.menezes@alunos.usj.edu.br>
2022-09-12 15:37:30 -0300

    Correção Titulo Vendas Por Hora Dia

--- src/components/SigVendasPorHoraDia.vue
+++ src/components/SigVendasPorHoraDia.vue
@@ -2,7 +2,7 @@
       <div class="uk-panel uk-card-default uk-margin-top" >
             <div class="uk-flex uk-flex-between uk-flex-middle  uk-padding-remove-bottom sig-header">
                   <span class="icon uk-margin-left"><i class="fa-solid fa-chart-column"></i></span>
-                  <span class="title uk-margin-top"><h3>Total em Vendas Por Hora <span class="uk-text-meta">valor em milhar</span></h3></span>
+                  <span class="title uk-margin-top"><h3>Total em Vendas Por Hora <span class="uk-text-meta">Em Milhares</span></h3></span>
                   <span class="badge"></span>
 
             </div>
